7e8b6367ec10de906b6d6dbbe630e290b3701f5c
Varis Vitols <varisvitols@gmail.com>
2025-08-22 17:23:42 +0300

    Use a publicly available backend service

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,7 +1,8 @@
 import { useCallback, useState } from 'react';
 import { Alert } from 'react-native';
 
-const API_URL = 'http://localhost:5001/api';
+// const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://react-native-finances.onrender.com/api';
 
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
